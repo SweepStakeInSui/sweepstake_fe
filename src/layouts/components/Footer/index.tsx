caa547c79ade3240817b90c96a91e7698f3389b4
croissain <nhattho03@gmail.com>
2024-07-28 17:56:08 +0700

    :recycle: update to new color config

--- src/layouts/components/Footer/index.tsx
+++ src/layouts/components/Footer/index.tsx
@@ -11,78 +11,60 @@ import HomeLogo from '../HomeLogo';
 
 export default function Footer() {
   return (
-    <footer className="py-10 border-solid border-t-[1px] border-dyb-20">
+    <footer className="py-10 border-solid border-t-[1px] border-borderMain">
       <Container>
         <Flex className="h-full justify-between items-start">
           <Stack>
             <HomeLogo />
-            <Typography.Text size={13} className="text-dyb-50">
+            <Typography.Text size={13} className="text-text-sublest">
               Copyright © 2024 SweepStack. All rights reserved.
             </Typography.Text>
           </Stack>
           <Flex className="gap-20 items-start">
             <Stack>
-              <Typography.Text size={12} className="text-dyb-50 px-2">
+              <Typography.Text size={12} className="text-text-sublest px-2">
                 COMPANY
               </Typography.Text>
               <Button variant="ghost" className="px-2 py-1">
-                <Typography.Text
-                  className="text-dyb-95 dark:text-wht-a90"
-                  size={14}
-                >
+                <Typography.Text className="text-text" size={14}>
                   Contract
                 </Typography.Text>
               </Button>
               <Button variant="ghost" className="px-2 py-1">
-                <Typography.Text
-                  className="text-dyb-95 dark:text-wht-a90"
-                  size={14}
-                >
+                <Typography.Text className="text-text" size={14}>
                   Join our Stack
                 </Typography.Text>
               </Button>
               <Button variant="ghost" className="px-2 py-1">
-                <Typography.Text
-                  className="text-dyb-95 dark:text-wht-a90"
-                  size={14}
-                >
+                <Typography.Text className="text-text" size={14}>
                   Privacy Policy
                 </Typography.Text>
               </Button>
             </Stack>
 
             <Stack>
-              <Typography.Text size={12} className="text-dyb-50 px-2">
+              <Typography.Text size={12} className="text-text-sublest px-2">
                 PRODUCTS
               </Typography.Text>
               <Button variant="ghost" className="px-2 py-1">
-                <Typography.Text
-                  className="text-dyb-95 dark:text-wht-a90"
-                  size={14}
-                >
+                <Typography.Text className="text-text" size={14}>
                   Viction Wallet (Extension)
                 </Typography.Text>
               </Button>
               <Button variant="ghost" className="px-2 py-1">
-                <Typography.Text
-                  className="text-dyb-95 dark:text-wht-a90"
-                  size={14}
-                >
+                <Typography.Text className="text-text" size={14}>
                   Viction Wallet (iOS)
                 </Typography.Text>
               </Button>
               <Button variant="ghost" className="px-2 py-1">
-                <Typography.Text
-                  className="text-dyb-95 dark:text-wht-a90"
-                  size={14}
-                >
+                <Typography.Text className="text-text" size={14}>
                   VicMaster
                 </Typography.Text>
               </Button>
             </Stack>
 
             <Stack>
-              <Typography.Text size={12} className="text-dyb-50 px-2">
+              <Typography.Text size={12} className="text-text-sublest px-2">
                 FOLLOW US
               </Typography.Text>
               <Flex className="gap-0">
