96c120cb369ddb82c532529bc14b9d6114fbc5f4
croissain <nhattho03@gmail.com>
2024-09-08 19:11:07 +0700

    :sparkles: update 404

---
+++ src/app/error.tsx
@@ -0,0 +1,29 @@
+'use client';
+
+import Image from 'next/image';
+import Link from 'next/link';
+import React from 'react';
+
+import Stack from '../components/common/Stack';
+import Typography from '../components/common/Typography';
+import { Button } from '../components/ui/button';
+
+const Error = () => {
+  return (
+    <div className="w-full h-screen flex justify-center items-center">
+      <Stack className="items-center gap-y-6">
+        <div className="relative w-[354px] h-[188px]">
+          <Image src="/images/404.png" alt="404" fill />
+        </div>
+        <Typography.Heading size={32} className="text-center">
+          Page Not Found
+        </Typography.Heading>
+        <Link href="/">
+          <Button variant="terriary">Back to Home</Button>
+        </Link>
+      </Stack>
+    </div>
+  );
+};
+
+export default Error;
