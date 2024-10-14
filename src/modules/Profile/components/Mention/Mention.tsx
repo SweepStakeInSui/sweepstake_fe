14b3b8f955c3e8e4ee9a1d2d4de57c2421514ac2
croissain <nhattho03@gmail.com>
2024-10-15 01:49:37 +0700

    :sparkles: update comment

--- src/modules/Profile/components/Mention/Mention.tsx
+++ src/modules/Profile/components/Mention/Mention.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import { CommentList } from '@/components/common/NestedComments';
 import { mockNestedComments } from '@/mocks/mockComments';
-import { TComment } from '@/services/markets/types';
+import type { TComment } from '@/services/markets/types';
 
 const Mention = () => {
   return (
