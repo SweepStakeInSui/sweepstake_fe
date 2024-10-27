5c07e96ecde16f9a189f4d25dc99c29071e18872
croissain <nhattho03@gmail.com>
2024-10-27 12:39:33 +0700

    :sparkles: update relate market

--- src/modules/MarketDetails/components/About/About.tsx
+++ src/modules/MarketDetails/components/About/About.tsx
@@ -4,36 +4,38 @@ import Svg from '@/components/common/Svg';
 import Typography from '@/components/common/Typography';
 
 interface IMarketsAboutProps {
-  desc: string;
+  desc?: string;
 }
 
 export default function MarketsAbout({ desc }: Readonly<IMarketsAboutProps>) {
   return (
-    <div>
-      <Typography.Heading size={24} className="mb-2">
-        About
-      </Typography.Heading>
-      <Typography.Text size={15} className="text-text-subtle mb-4">
-        {desc}
-      </Typography.Text>
-
-      <Flex className="p-3 items-start rounded-md bg-bg-sublest border border-borderSublest">
-        <Svg src="/icons/link.svg" className="size-5" />
-        <Typography.Text size={15} className="text-text-subtle">
-          Sources from{' '}
-          <SourceLinks
-            sources={[
-              { title: 'GitHub', url: 'https://github.com' },
-              { title: 'DataHub.io', url: 'https://datahub.io' },
-              { title: 'Data.gov', url: 'https://data.gov' },
-              {
-                title: 'U.S. Energy Information Administration (EIA)',
-                url: 'https://www.eia.gov',
-              },
-            ]}
-          />
+    desc && (
+      <div>
+        <Typography.Heading size={24} className="mb-2">
+          About
+        </Typography.Heading>
+        <Typography.Text size={15} className="text-text-subtle mb-4">
+          {desc}
         </Typography.Text>
-      </Flex>
-    </div>
+
+        <Flex className="p-3 items-start rounded-md bg-bg-sublest border border-borderSublest">
+          <Svg src="/icons/link.svg" className="size-5" />
+          <Typography.Text size={15} className="text-text-subtle">
+            Sources from{' '}
+            <SourceLinks
+              sources={[
+                { title: 'GitHub', url: 'https://github.com' },
+                { title: 'DataHub.io', url: 'https://datahub.io' },
+                { title: 'Data.gov', url: 'https://data.gov' },
+                {
+                  title: 'U.S. Energy Information Administration (EIA)',
+                  url: 'https://www.eia.gov',
+                },
+              ]}
+            />
+          </Typography.Text>
+        </Flex>
+      </div>
+    )
   );
 }
