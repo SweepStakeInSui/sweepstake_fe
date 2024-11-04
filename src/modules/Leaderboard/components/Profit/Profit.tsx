872ef446ee658fe01f2a050cbe03b9a4f8515b89
croissain <nhattho03@gmail.com>
2024-11-04 23:42:20 +0700

    :sparkles: update leaderboard

--- src/modules/Leaderboard/components/Profit/Profit.tsx
+++ src/modules/Leaderboard/components/Profit/Profit.tsx
@@ -1,12 +1,21 @@
 import Flex from '@/components/common/Flex';
 import Svg from '@/components/common/Svg';
 import Typography from '@/components/common/Typography';
-import { mockTopVolumeThisWeek } from '@/mocks/mockTopVolumeThisWeek';
-import { TopVolumeItem } from '@/modules/Home/components/TopVolume';
 
 import Wraper from '../Wraper';
+import { useQuery } from '@tanstack/react-query';
+import { LeaderboardServices } from '@/services/leaderboard';
+import { LeaderboardItem } from '@/modules/Leaderboard/components/LeaderboardItem';
 
 const ProfitLeaderboard = () => {
+  const { data: profitLeaderboardData } = useQuery({
+    queryKey: ['profit-leaderboard'],
+    queryFn: async () =>
+      LeaderboardServices.getLeaderboardProfit({
+        page: 1,
+        limit: 10,
+      }),
+  });
   return (
     <Wraper>
       <Flex className="bg-pu-5 dark:bg-[rgba(155,1,245,0.08)] py-3 px-2">
@@ -15,8 +24,15 @@ const ProfitLeaderboard = () => {
           Profit
         </Typography.Heading>
       </Flex>
-      {mockTopVolumeThisWeek.map((item) => (
-        <TopVolumeItem key={item.id} {...item} className="py-3" />
+      {profitLeaderboardData?.data?.items?.map((item, index) => (
+        <LeaderboardItem
+          key={item.id}
+          username={item.username || item.address}
+          avatar={item.avatar}
+          price={item.volume}
+          rank={index + 1}
+          className="py-3"
+        />
       ))}
     </Wraper>
   );
--- src/modules/Leaderboard/components/Volume/Volume.tsx
+++ src/modules/Leaderboard/components/Volume/Volume.tsx
@@ -1,12 +1,21 @@
 import Flex from '@/components/common/Flex';
 import Svg from '@/components/common/Svg';
 import Typography from '@/components/common/Typography';
-import { mockTopVolumeThisWeek } from '@/mocks/mockTopVolumeThisWeek';
-import { TopVolumeItem } from '@/modules/Home/components/TopVolume';
 
 import Wraper from '../Wraper';
+import { LeaderboardServices } from '@/services/leaderboard';
+import { useQuery } from '@tanstack/react-query';
+import { LeaderboardItem } from '@/modules/Leaderboard/components/LeaderboardItem';
 
 const VolumeLeaderboard = () => {
+  const { data: volumeLeaderboardData } = useQuery({
+    queryKey: ['volume-leaderboard'],
+    queryFn: async () =>
+      LeaderboardServices.getLeaderboardVolume({
+        page: 1,
+        limit: 10,
+      }),
+  });
   return (
     <Wraper>
       <Flex className="bg-org-5 dark:bg-bagde-warn py-3 px-2">
@@ -15,8 +24,15 @@ const VolumeLeaderboard = () => {
           Volume
         </Typography.Heading>
       </Flex>
-      {mockTopVolumeThisWeek.map((item) => (
-        <TopVolumeItem key={item.id} {...item} className="py-3" />
+      {volumeLeaderboardData?.data?.items?.map((item, index) => (
+        <LeaderboardItem
+          key={item.id}
+          username={item.username || item.address}
+          avatar={item.avatar}
+          price={item.volume}
+          rank={index + 1}
+          className="py-3"
+        />
       ))}
     </Wraper>
   );
